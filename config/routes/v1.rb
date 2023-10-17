e94ee5eb2f68a1b0a005b82bf2aa92bb587402d1
neongrau <neongrau@gmail.com>
2023-10-17 15:54:10 +0200

    endpoint to have the current user leave a tenant
    - drops all access to the tenant
    - and silence rubocop with opinions

--- config/routes/v1.rb
+++ config/routes/v1.rb
@@ -1,18 +1,18 @@
 namespace :v1 do
-
+  # begin
   namespace :access_control do
     scope module: :app, path: 'app/:name' do
-      resources :groups, only: [:index]
+      resources :groups, only: %i(index)
     end
     resources :apps, module: :apps, only: [] do
-      resources :groups, only: [:index]
+      resources :groups, only: %i(index)
       resources :tenant, module: :tenant, only: [] do
-        resources :users, only: [:index, :show, :update, :destroy]
+        resources :users, only: %i(index show update destroy)
       end
     end
     resources :tenant, module: :tenant, only: [] do
-      resources :groups, only: [:index]
-      resources :users, only: [:index, :show, :update, :destroy]
+      resources :groups, only: %i(index)
+      resources :users, only: %i(index show update destroy)
     end
   end
 
@@ -20,74 +20,73 @@ namespace :v1 do
   resources :email_blacklist
   resources :app_admin
 
-  resources :apps, module: :apps, only: []  do
+  resources :apps, module: :apps, only: [] do
     namespace :picker do
       resources :roles, only: [] do
-        resources :user_organization, module: :roles, only: [:index, :show]
-        resources :functionalities, module: :roles, only: [:index, :show]
+        resources :user_organization, module: :roles, only: %i(index show)
+        resources :functionalities, module: :roles, only: %i(index show)
       end
       resources :functionalities, only: [] do
-        resources :roles, module: :functionalities, only: [:index, :show]
+        resources :roles, module: :functionalities, only: %i(index show)
       end
       resources :users, only: [] do
-        resources :groups, module: :users, only: [:index, :show]
+        resources :groups, module: :users, only: %i(index show)
       end
-      resources :user_organization, only: [:index]
+      resources :user_organization, only: %i(index)
     end
-    resources :groups, only: [:index, :show]
+    resources :groups, only: %i(index show)
     resources :functionalities
     resources :functionalities, module: :functionalities, path: :functionalities do
       resources :roles, only: %i(index show create destroy)
     end
     resources :roles
     resources :roles, module: :roles, path: :roles do
-      resources :actors, only: [:index, :show]
-      resources :actor_roles, only: [:index, :show, :create, :destroy]
+      resources :actors, only: %i(index show)
+      resources :actor_roles, only: %i(index show create destroy)
       resources :functionalities, only: %i(index show create destroy)
     end
-    resources :users, only: [:index, :show, :update, :destroy] do
-      resources :functionalities, module: :users, only: [:index, :show]
-      resources :roles, module: :users, only: [:index, :show]
-      resources :actors, module: :users, only: [:index, :show, :create, :destroy]
-      resources :tenants, module: :users, only: [:index, :show]
+    resources :users, only: %i(index show update destroy) do
+      resources :functionalities, module: :users, only: %i(index show)
+      resources :roles, module: :users, only: %i(index show)
+      resources :actors, module: :users, only: %i(index show create destroy)
+      resources :tenants, module: :users, only: %i(index show)
     end
     resources :organizations
-    resources :organizations_tree, only: [:index, :show]
+    resources :organizations_tree, only: %i(index show)
     resources :organizations, module: :organizations, path: 'organizations' do
-      resources :actor_roles, only: [:index, :show, :create, :destroy]
+      resources :actor_roles, only: %i(index show create destroy)
     end
     resources :contents
     resources :tenants
     resources :tenants, module: :tenants, only: [] do
       resources :organizations
-      resources :organizations_tree, only: [:index, :show]
+      resources :organizations_tree, only: %i(index show)
       resources :organizations, module: :organizations, only: [] do
-        resources :actor_roles, only: [:index, :show, :create, :destroy]
-        resources :mappable_users, only: [:index], module: :picker, path: 'picker/mappable_users'
-        resources :mappings, only: [:index, :show, :create, :destroy]
+        resources :actor_roles, only: %i(index show create destroy)
+        resources :mappable_users, only: %i(index), module: :picker, path: 'picker/mappable_users'
+        resources :mappings, only: %i(index show create destroy)
       end
-      resources :users, only: [:index, :show, :update] do
-        resources :functionalities, module: :users, only: [:index, :show]
-        resources :roles, module: :users, only: [:index, :show]
-        resources :actors, module: :users, only: [:index, :show, :create, :destroy]
-        #resources :tenants, module: :users, only: [:index, :show]
+      resources :users, only: %i(index show update) do
+        resources :functionalities, module: :users, only: %i(index show)
+        resources :roles, module: :users, only: %i(index show)
+        resources :actors, module: :users, only: %i(index show create destroy)
       end
       namespace :picker do
-        resources :user_organization, only: [:index]
-        resources :mappable_users, only: [:index]
+        resources :user_organization, only: %i(index)
+        resources :mappable_users, only: %i(index)
         resources :users, only: [] do
-          resources :groups, module: :users, only: [:index, :show], as: :v1_apps_tenants_picker_users_groups
+          resources :groups, module: :users, only: %i(index show), as: :v1_apps_tenants_picker_users_groups
         end
       end
     end
   end
 
   namespace :picker do
-    resources :actors, only: [:index] do
-      resources :mappings, only: [:index, :show]
+    resources :actors, only: %i(index) do
+      resources :mappings, only: %i(index show)
     end
     resources :tenant, only: [] do
-      resources :users, only: [:index, :show, :update], controller: :tenant_users
+      resources :users, only: %i(index show update), controller: :tenant_users
     end
   end
 
@@ -97,7 +96,6 @@ namespace :v1 do
   end
 
   scope module: :app, path: '*app' do
-    # resource :user, only: [:index, :update, :destroy]
     get 'user' => 'user#index'
     put 'user' => 'user#update'
     delete 'user' => 'user#destroy'
@@ -106,22 +104,24 @@ namespace :v1 do
     resources :content_acceptance, only: [:update]
 
     scope path: ':tenant_id', module: :tenant do
-      resources :invitations, only: [:create, :update, :destroy], constraints: { id: /[a-z0-9\{\}]*/ }
+      resources :invitations, only: %i(create update destroy), constraints: { id: /[a-z0-9\{\}]*/ }
     end
   end
 
   namespace :user do
-    resources :apps, only: [:index]
-    resources :account_activity, only: [:index]
-    resources :account_logins, only: [:index, :destroy]
-    resources :quits, only: [:destroy]
-    resources :authenticate_otp, only: [:create]
-    resources :tenants, path: :tenant, only: [:index, :create, :show, :update, :destroy]
+    resources :apps, only: %i(index)
+    resources :account_activity, only: %i(index)
+    resources :account_logins, only: %i(index destroy)
+    resources :quits, only: %i(destroy)
+    resources :authenticate_otp, only: %i(create)
+    resources :tenants, path: :tenant, only: %i(index create show update destroy)
+    resources :leave_tenant, only: %i(destroy)
+
     put 'invitations/:id' => 'invitations#update', constraints: { id: /[a-z0-9]*/ }, as: 'invitation_accept', noswagger: true
   end
 
   # Maintenance checks
-  get 'server_under_maintenance' => "general#server_under_maintenance", noswagger: true
+  get 'server_under_maintenance' => 'general#server_under_maintenance', noswagger: true
   get 'maintenance' => 'general#maintenance', noswagger: true
-
+  # end
 end
